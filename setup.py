21c55a7b5e3111ec29eeb592903c49194b3d6c35
Paul Diakov <paul.diakov@gmail.com>
2014-10-27 16:44:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(name="torminify",
     install_requires=['pyyaml>=3.11','tornado>=3.0.2'],
     version="0.1.3",
-    description="",
+    description="Module for Tornado Web Framework, designed to automate minification of css and js files, easily implement asynchronous loading of scripts and additional stylesheets, and cache compiled tornado templates in memory",
     author="Paul Diakov",
     author_email='paul.diakov@gmail.com',
     platforms=["any"],
